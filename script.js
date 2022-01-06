d5235c3dc86df3dab65b95797ea50b36c01c1240
nokko <13428215+nok-ko@users.noreply.github.com>
2022-01-05 21:21:03 -0800

    Refactor stuff OOP-ly. Start work on a new layout…

--- script.js
+++ script.js
@@ -3,72 +3,153 @@ const tableEl = document.getElementsByTagName("table")[0];
 const container = document.getElementsByTagName("tbody");
 const tableBodyEl = tableEl.getElementsByTagName("tbody")[0]
 
-// n-wide truth table
-function makeTable(n) {
-	const table = [];
-	for (let i = 0; i < n ** 2; i++) {
-		const row = [];
-		// [i >> 3 & 1, i >> 2 & 1, i >> 1 & 1, i >> 0 & 1])
-		for (let j = (n - 1); j >= 0; j--) {
-			row.push(i >> j & 1);
-		}
-		table.push(row);
-	}
-	return table;
-}
 
-// Slam it into the DOM
-function writeTable(table) {
-	for (let row of table) {
-		const rowEl = document.createElement("tr");
-		for (let value of row) {
-			const valueEl = document.createElement("td");
-			valueEl.textContent = value;
-			if (value == 1) {
-				// valueEl.classList.add("true");
+class TruthTable {
+	/**
+	 * @param  {Number} width
+	 * @param  {HTMLTableElement} tableEl
+	 */
+	constructor(width, tableEl) {
+		this.width = width
+		this.data = this.createBaseValues(width)
+		this.rootEl = tableEl
+		/** @param {HTMLTableSectionElement} */
+		this.bodyEl = tableEl.getElementsByTagName("tbody")[0]
+		this.refresh()
+
+		/** @type {Column[]} */
+		this.additionalCols = []
+	}
+
+	/**
+	 * Creates an n-wide truth table: an array of the numbers [0, 2^n), 
+	 * where each element is itself an array of the number's digits.
+	 * For example, `createBaseValues(3)` results in: `[ [0,0,0], [0,0,1], [0,1,0], …, [1,1,1] ]`
+	 * @param   {Number} n - how many variables should be in each row
+	 * @returns {Number[][]}
+	 */
+	createBaseValues(n) {
+		const table = []
+		for (let i = 0; i < (2 ** n); i++) {
+			const row = []
+			// [i >> 3 & 1, i >> 2 & 1, i >> 1 & 1, i >> 0 & 1])
+			for (let j = (n - 1); j >= 0; j--) {
+				row.push(i >> j & 1)
 			}
-			rowEl.appendChild(valueEl);
+			table.push(row)
 		}
-		tableBodyEl.appendChild(rowEl);
+		return table
+	}
+
+	/**
+	 * Updates the DOM representation of this `TruthTable` to match the internal `data`.
+	 * @param  {Number[][]} data
+	 * @param  {HTMLElement} baseEl
+	 */
+	refresh() {
+		// Clear out the element before writing
+		this.bodyEl.textContent = ""
+
+		const frag = document.createDocumentFragment()
+		for (const row of this.data) {
+			const rowEl = document.createElement("tr");
+			for (const [idx, value] of row.entries()) {
+				const valueEl = document.createElement("td");
+				valueEl.textContent = value;
+				if (value == 1 && idx >= this.width) {
+					valueEl.classList.add("true");
+				}
+				rowEl.appendChild(valueEl);
+			}
+			frag.appendChild(rowEl)
+		}
+		this.bodyEl.appendChild(frag);
+	}
+
+
+	/**
+	 * Evil regex fn that converts BC'D' notation to
+	 * JS Notation. Parens get yeeted, whoops.
+	 * @param {String} exprString
+	 * @returns {String}
+	 */
+	static parseExpr(exprString) {
+		// A' -> 'A
+		const a = exprString.replace(/([A-Z])(')?/g, "$2$1")
+		// 'A -> !A
+		const b = a.replace(/'/g, "!")
+		// !A!B!C + AD -> ["!A!B!C", "AD"]
+		try {
+			const terms = b.split(/\s*\+\s*/)
+				.map(
+					// ["!A!B!C", "AD"] -> ["!A&&!B&&!C", "A&&D"] 
+					p => p.match(/!?[A-Z]/g)
+						.join("&&")
+				)
+			// ["!A&&!B&&!C", "A&&D"]  -> "!A&&!B&&!C || A&&D"
+			const rejoined = terms.join(" || ")
+			return rejoined
+		} catch (exception) {
+			if (exception.name == "TypeError") {
+				// If the match() doesn't find anything, JS will throw a
+				// TypeError. Not catastrophic, just means the expr string
+				// couldn't be parsed. 
+				// console.debug("Incomplete expr string?")
+			}
+			// Fail silently, we'll get 'em next time.
+			return ""
+		}
+
+	}
+
+	/**
+	 * Run the specified exprString on `terms`
+	 * @param {String} exprString
+	 * @param {Array<Number>} terms
+	 */
+	static runExpr(exprString, terms) {
+		const fn = new Function(`
+		const [A, B, C, D, E, F, G, H, I, J, K, L] = arguments
+		return Number(${exprString})`)
+		//   console.log(fn.apply(this, terms))
+		return fn.apply(this, terms)
+	}
+
+	/**
+	 * Add a column to internal state of this `TruthTable`, fill it with
+	 * the result of running `expr` on each row, and refresh the visual
+	 * representation of the table.
+	 * @param  {Function} expr
+	 */
+	addExprValues(expr) {
+		for (let row of this.data) {
+			const calculatedVal = expr(row);
+			row.push(calculatedVal);
+		}
+		this.refresh()
+	}
+
+	/** 
+	 * Add the heading part of a column to the this `TruthTable`'s 
+	 * `<thead>` element.
+	 * @param {String} text - the text to put in the column's heading
+	 */
+	addCol(text) {
+		const headRowEl = this.rootEl.querySelector("thead>tr");
+		const valueEl = document.createElement("td");
+		valueEl.textContent = text;
+		headRowEl.appendChild(valueEl);
 	}
 }
 
-/**
- * Evil regex fn that converts BC'D' notation to
- * JS Notation. Parens get yeeted, whoops.
- * @param {String} exprString
- * @returns {String}
- **/
-function parseExpr(exprString) {
-	// A' -> 'A
-	const a = exprString.replace(/([A-Z])(')?/g, "$2$1")
-	// 'A -> !A
-	const b = a.replace(/'/g, "!")
-	// !A!B!C + AD -> ["!A!B!C", "AD"]
-	const terms = b.split(/\s*\+\s*/)
-		.map(
-			// ["!A!B!C", "AD"] -> ["!A&&!B&&!C", "A&&D"] 
-			p => p.match(/!?[A-Z]/g)
-			.join("&&")
-		)
-	// ["!A&&!B&&!C", "A&&D"]  -> "!A&&!B&&!C || A&&D"
-	const rejoined = terms.join(" || ")
-	return rejoined
+class Column {
+	constructor(heading, rootEl, values) {
+		this.heading = heading
+		this.values = values
+		this.rootEl = rootEl
+	}
 }
 
-/**
- * Run the specified exprString on `terms`
- * @param {String} exprString
- * @param {Array<Number>} terms
- */
-function runExpr(exprString, terms) {
-	const fn = new Function(`
-	  const [A, B, C, D, E, F, G, H, I, J, K, L] = arguments
-	  return Number(${exprString})
-	  `)
-	//   console.log(fn.apply(this, terms))
-	return fn.apply(this, terms)
-}
 /**
  * @param  {number[][]} tableData
  */
@@ -78,13 +159,12 @@ function addInteractiveCol(tableData) {
 	colHeadEl.appendChild(colHeadInput)
 	tableEl.querySelector("thead > tr").appendChild(colHeadEl)
 
-	const column = []
-
-	for (const rowEl of tableBodyEl.children) {
-		const blankDataEl = document.createElement("td")
-		rowEl.appendChild(blankDataEl)
-		column.push(blankDataEl)
+	const colIdx = tableData[0].length
+	for (const row of tableData) {
+		row.push(0)
 	}
+	// refresh after update
+	writeTable(tableData, tableBodyEl, true)
 
 	colHeadInput.addEventListener("keydown", (e) => {
 		if (e.key == "Enter") {
@@ -97,37 +177,30 @@ function addInteractiveCol(tableData) {
 
 	colHeadInput.addEventListener("input", function (e) {
 		exprString = parseExpr(this.value.toUpperCase())
-		for (const [idx, cellEl] of column.entries()) {
-			const result = runExpr(exprString, tableData[idx])
-			if (result == 1) {
-				cellEl.classList.add("true");
-			} else {
-				cellEl.classList.remove("true");
-			}
-			cellEl.textContent = result;
+		for (const row of tableData) {
+			const result = runExpr(exprString, row)
+			row[colIdx] = result
 		}
+		// refresh after update
+
+		// for (const [idx, cellEl] of column.entries()) {
+		// 	const result = runExpr(exprString, tableData[idx])
+		// 	if (result == 1) {
+		// 		cellEl.classList.add("true");
+		// 	} else {
+		// 		cellEl.classList.remove("true");
+		// 	}
+		// 	cellEl.textContent = result;
+		// }
 	})
 }
 
-function addExprValues(expr) {
-	let i = 0;
-	for (let node of tableBodyEl.children) {
-		const calculatedVal = expr(values[i]);
-		const exprEl = document.createElement("td");
-		exprEl.textContent = calculatedVal;
-		if (calculatedVal == "1") {
-			exprEl.classList.add("true");
-		}
-		node.appendChild(exprEl);
-		i++;
-	}
-}
 
 function addBackRefExprValues(expr) {
 	let i = 0;
-	for (let node of tableBodyEl.children) {
+	for (let rowEl of tableBodyEl.children) {
 		const backReferences = [];
-		for (let neighbour of node.childNodes) {
+		for (let neighbour of rowEl.childNodes) {
 			backReferences.push(neighbour.textContent == "1");
 		}
 
@@ -137,63 +210,64 @@ function addBackRefExprValues(expr) {
 		if (calculatedVal == "1") {
 			exprEl.classList.add("true");
 		}
-		node.appendChild(exprEl);
+		rowEl.appendChild(exprEl);
 		i++;
 	}
 }
 
 
-function addCol(text) {
-	const headRowEl = document.querySelector("thead>tr");
-	const valueEl = document.createElement("td");
-	valueEl.textContent = text;
-	headRowEl.appendChild(valueEl);
-}
 
+// // const values = makeTable(4);
+// writeTable(values, tableBodyEl);
 
-const values = makeTable(4);
-writeTable(values);
-
-function writeAllTerms() {
-	addCol("BC'D'");
-	addExprValues((values => {
+function writeAllTerms(truthTable) {
+	truthTable.addCol("BC'D'");
+	truthTable.addExprValues((values => {
 		const [a, b, c, d] = values;
 		return (b && !c && !d) ? "1" : "0";
 	}));
 
-	addCol("ABC");
-	addExprValues((values => {
+	truthTable.addCol("ABC");
+	truthTable.addExprValues((values => {
 		const [a, b, c] = values;
 		return (a && b && c) ? "1" : "0";
 	}));
 
-	addCol("AC'D");
-	addExprValues((values => {
+	truthTable.addCol("AC'D");
+	truthTable.addExprValues((values => {
 		const [a, b, c, d] = values;
 		return (a && !c && d) ? "1" : "0";
 	}));
 
-	addCol("AB'D");
-	addExprValues((values => {
+	truthTable.addCol("AB'D");
+	truthTable.addExprValues((values => {
 		const [a, b, c, d] = values;
 		return (a && !b && d) ? "1" : "0";
 	}));
 
-	addCol("A'BD'");
-	addExprValues((values => {
+	truthTable.addCol("A'BD'");
+	truthTable.addExprValues((values => {
 		const [a, b, c, d] = values;
 		return (!a && b && !d) ? "1" : "0";
 	}));
 
-	addCol("Q");
-	addBackRefExprValues((values, backs) => {
-		const [a, b, c, d] = values;
-		// console.log("backs:");
-		// console.log(backs);
-		return (backs[4] || backs[5] || backs[6] || backs[7] || backs[8]) ?
-			"1" : "0";
-	});
+	// addCol("Q");
+	// addBackRefExprValues((values, backs) => {
+	// 	const [a, b, c, d] = values;
+	// 	// console.log("backs:");
+	// 	// console.log(backs);
+	// 	return (backs[4] || backs[5] || backs[6] || backs[7] || backs[8]) ?
+	// 		"1" : "0";
+	// });
 }
 
-// writeAllTerms();
-addInteractiveCol(values);
+const truthTable = new TruthTable(4, tableEl)
+writeAllTerms(truthTable);
+// addInteractiveCol(values);
+/**
+ * Add a Karnaugh map element to the page.
+ * @param  {} data
+ */
+function addKMap(data) {
+
+}
